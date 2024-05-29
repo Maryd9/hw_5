5ba212b5487343a0ce9e364d9dd8487de5aef9f7
masha <masha-krim@yandex.ru>
2024-05-30 01:35:52 +0500

    first commit

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+from selene import browser
+
+
+@pytest.fixture(scope='function', autouse=True)
+def browser_managment():
+    browser.config.base_url = 'https://demoqa.com/automation-practice-form'
+    browser.config.window_width = 1366
+    browser.config.window_height = 768
+    browser.open('/')
+
+    yield
+
+    browser.quit()
